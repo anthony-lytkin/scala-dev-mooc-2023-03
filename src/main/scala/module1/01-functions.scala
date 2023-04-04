b9c5db1323ea30c667ef2a7d6fffd8f0c0f9566b
anthony-lytkin <anthony.lytkin@gmail.com>
2023-04-04 22:41:02 +0300

    Module 1. Extra exercises.

--- src/main/scala/module1/01-functions.scala
+++ src/main/scala/module1/01-functions.scala
@@ -40,15 +40,15 @@ object functions {
     case x if x._2 != 0 => x._1 / x._2
   }
 
-  divide.isDefinedAt()
-  divide.apply()
+//  divide.isDefinedAt()
+//  divide.apply()
 
   val divide2: PartialFunction[(Int, Int), Int] = new PartialFunction[(Int, Int), Int] {
     override def isDefinedAt(x: (Int, Int)): Boolean = x._2 != 0
 
-    override def apply(v1: (Int, Int)): Int = x._1 / x._2
+    override def apply(v1: (Int, Int)): Int = v1._1 / v1._2
   }
-  divide2.isDefinedAt()
+//  divide2.isDefinedAt()
 
   val r5: List[Int] = List((1, 2), (2, 0)).collect(divide)
 
@@ -77,16 +77,24 @@ object functions {
    *  Задание 1. Написать ф-цию метод isEven, которая будет вычислять является ли число четным
    */
 
+  def isEven(x: Int): Boolean = if (x % 2 == 0) true else false
+
 
   /**
    * Задание 2. Написать ф-цию метод isOdd, которая будет вычислять является ли число нечетным
    */
 
+  def isOdd(x: Int): Boolean = if (x % 2 == 1) true else false
+  def isOdd1(x: Int): Boolean = !isEven(x)
 
   /**
    * Задание 3. Написать ф-цию метод filterEven, которая получает на вход массив чисел и возвращает массив тех из них,
    * которые являются четными
    */
+
+  val lst: List[Int] = List(1, 2, 3, 4, 5, 6, 7)
+  def filterEven(lst: List[Int]): List[Int] = lst.filter(isEven)
+  def filterEven1(lst: List[Int]): List[Int] = lst.collect { case n if isEven(n) => n }
 
 
 
@@ -94,6 +102,8 @@ object functions {
    * Задание 4. Написать ф-цию метод filterOdd, которая получает на вход массив чисел и возвращает массив тех из них,
    * которые являются нечетными
    */
+
+  def filterOdd(lst: List[Int]): List[Int] = lst.collect { case n if isOdd(n) => n }
 
 
   /**
