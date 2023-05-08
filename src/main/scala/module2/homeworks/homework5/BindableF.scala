fa948ee0c937d6258b2e35ee41f6b9770af8b365
Anthony <67843304+anthony-lytkin@users.noreply.github.com>
2023-05-08 20:58:39 +0300

    Scala Otus Module2. Homework 5. tupleF

---
+++ src/main/scala/module2/homeworks/homework5/BindableF.scala
@@ -0,0 +1,25 @@
+package module2.homeworks.homework5
+
+import scala.language.implicitConversions
+
+trait BindableF[F[_], A]{
+
+  def map[B](f: A => B): F[B]
+  def flatMap[B](f: A => F[B]): F[B]
+
+}
+
+object BindableF {
+
+  implicit class BinderOption[A](a: Option[A]) extends BindableF[Option, A] {
+    override def map[B](f: A => B): Option[B] = a.map(f)
+    override def flatMap[B](f: A => Option[B]): Option[B] = a.flatMap(f)
+  }
+
+  implicit class BindList[A](a: List[A]) extends BindableF[List, A] {
+    override def map[B](f: A => B): List[B] = a.map(f)
+    override def flatMap[B](f: A => List[B]): List[B] = a.flatMap(f)
+  }
+
+}
+
